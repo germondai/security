9da34dc9e26c03e3633bcddb98698906c9e2859f
germondai <germondai@gmail.com>
2026-04-25 09:08:31 +0200

    feat(security): improve analyzers

--- packages/security/src/analyzers/index.ts
+++ packages/security/src/analyzers/index.ts
@@ -1,4 +1,9 @@
-export { shannonEntropy, detectPoolSize, naiveEntropyBits } from './entropy.js';
-export { detectPatterns, type DetectedPattern } from './patterns.js';
-export { estimateCrackTimes, estimateCrackTimesFromBits, SCENARIO_GPS, type CrackTimeEstimate } from './cracktime.js';
-export { analyzePassword, type StrengthResult } from './strength.js';
+export {
+  type CrackTimeEstimate,
+  estimateCrackTimes,
+  estimateCrackTimesFromBits,
+  SCENARIO_GPS,
+} from "./cracktime.js";
+export { detectPoolSize, naiveEntropyBits, shannonEntropy } from "./entropy.js";
+export { type DetectedPattern, detectPatterns } from "./patterns.js";
+export { analyzePassword, type StrengthResult } from "./strength.js";
